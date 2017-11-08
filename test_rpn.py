5608439dc5be2e85afc20974d3a6a7b7b0e15558
Raymond Pressly <raynicho@umich.edu>
2017-11-08 14:13:03 -0500

    Fixed tabs vs spaces

--- test_rpn.py
+++ test_rpn.py
@@ -16,5 +16,5 @@ class TestBasics(unittest.TestCase):
         result = rpn.calculate("6 3 /")
         self.assertEqual(2, result)
     def test_carat(self):
-	result = rpn.calculate("6 3 ^")
-	self.assertEqual(216, result)
+        result = rpn.calculate("6 3 ^")
+        self.assertEqual(216, result)
